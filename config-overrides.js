fac93294bfc8d1c85505d97b3ae1137402cf7cd9
Yago Gonzalez <jagogc@gmail.com>
2018-08-02 13:28:42 +0100

    Add rewire config

---
+++ config-overrides.js
@@ -0,0 +1,11 @@
+const { compose } = require("react-app-rewired");
+const { createEmotionRewire } = require("react-app-rewire-emotion");
+ /* config-overrides.js */
+module.exports = function override(config, env) {
+  const rewires = compose(
+    //createRewireForSomeOtherPlugin(),
+    // ... place more rewires
+    createEmotionRewire({})
+  );
+  return rewires(config, env);
+};
