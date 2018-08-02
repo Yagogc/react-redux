0b3318048550f28920bf0673acaa049469a289f6
Yago Gonzalez <jagogc@gmail.com>
2018-08-02 16:09:36 +0100

    Add Redux Actions

---
+++ src/state/Actions.js
@@ -0,0 +1,38 @@
+import {
+  ADD_TODO,
+  REMOVE_TODO,
+  TOGGLE_TODO,
+  ADD_GOAL,
+  REMOVE_GOAL
+} from "./Types";
+
+export const addTodoAction = todo => {
+  return {
+    type: ADD_TODO,
+    todo
+  };
+};
+export const removeTodoAction = id => {
+  return {
+    type: REMOVE_TODO,
+    id
+  };
+};
+export const toggleTodoAction = id => {
+  return {
+    type: TOGGLE_TODO,
+    id
+  };
+};
+export const addGoalAction = goal => {
+  return {
+    type: ADD_GOAL,
+    goal
+  };
+};
+export const removeGoalAction = id => {
+  return {
+    type: REMOVE_GOAL,
+    id
+  };
+};
