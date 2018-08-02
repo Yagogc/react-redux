59720984e73fb0a15e1ff4479abe34fae7d41588
Yago Gonzalez <jagogc@gmail.com>
2018-08-02 17:11:13 +0100

    Update App with the api state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Goals from "./components/Goals";
 import Todos from "./components/Todos";
 import { todos, goals } from "./state/Reducers";
 import { logger } from "./state/Logger";
+import API from "./utils/api";
+import { receiveDataAction } from "./state/Actions";
 
 const store = createStore(
   combineReducers({
@@ -15,6 +17,12 @@ const store = createStore(
 class App extends Component {
   componentDidMount() {
     store.subscribe(() => this.forceUpdate());
+    console.log(API);
+    Promise.all([API.fetchTodos(), API.fetchGoals()]).then(([todos, goals]) => {
+      console.log({ todos });
+      console.log({ goals });
+      store.dispatch(receiveDataAction(todos, goals));
+    });
   }
   render() {
     const { todos, goals } = store.getState();
