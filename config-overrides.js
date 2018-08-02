30ec2e3485745f6d11e5322dcb37c8c48bdf17cc
Yago Gonzalez <jagogc@gmail.com>
2018-08-02 13:37:37 +0100

    Linting

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,6 @@
 const { compose } = require("react-app-rewired");
 const { createEmotionRewire } = require("react-app-rewire-emotion");
- /* config-overrides.js */
+/* config-overrides.js */
 module.exports = function override(config, env) {
   const rewires = compose(
     //createRewireForSomeOtherPlugin(),
