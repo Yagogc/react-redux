e36dccb9f46224ca78bda418a29cc6f3f6a2b48c
Yago Gonzalez <jagogc@gmail.com>
2018-08-02 15:28:17 +0100

    Add List component

--- src/components/Goals.js
+++ src/components/Goals.js
@@ -1,13 +1,19 @@
 import React, { Component } from "react";
+import List from "./List";
 
 class Goals extends Component {
+  textInput = React.createRef();
+
+  addGoal = () => {
+    console.log(this.textInput.current.value);
+  };
   render() {
     return (
       <React.Fragment>
         <h1>Goal List</h1>
-        <input type="text" />
-        <button>Add Goal</button>
-        <ul />
+        <input type="text" placeholder="Add Goal" ref={this.textInput} />
+        <button onClick={() => this.addGoal()}>Add Goal</button>
+        <List />
       </React.Fragment>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import List from "./List";
 
 class Todos extends Component {
   render() {
     return (
       <React.Fragment>
         <h1>Todo List</h1>
-        <input type="text" />
+        <input type="text" placeholder="Add todo" />
         <button>Add Todo</button>
-        <ul />
+        <List />
       </React.Fragment>
     );
   }
