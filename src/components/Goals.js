a521e683763ac5dd56f7e0445a8fb49f768fc161
Yago Gonzalez <jagogc@gmail.com>
2018-08-02 13:38:04 +0100

    Add initial UI - Goals - Todos

---
+++ src/components/Goals.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Goals extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Goal List</h1>
+        <input type="text" />
+        <button>Add Goal</button>
+        <ul />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Goals;
---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+class Todos extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Todo List</h1>
+        <input type="text" />
+        <button>Add Todo</button>
+        <ul />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Todos;
